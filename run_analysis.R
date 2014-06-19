530e0d194c9aa2e05b81704d58dcf724d48ec7b3
michaels-git <michaels-git@fastmail.fm>
2014-06-18 21:23:53 -0700

    second commit

--- run_analysis.R
+++ run_analysis.R
@@ -10,5 +10,19 @@ download.file(url = fileURL, destfile = filename, method = "wget")
 if(!file.exists("UCI HAR Dataset")) {
 unzip(filename)
 }
-test_data <- read.table("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
-training_data <- read.table("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
+# read in the column names... V1 is just a sequence number, but V2
+# has the text of the column names (use those when reading in the
+# data)
+column_names <- read.table("UCI HAR Dataset/features.txt", sep="", header=FALSE)
+# clean up the column names a bit
+column_names$V2 <- gsub("-", "", column_names$V2)
+column_names$V2 <- gsub(",", "", column_names$V2)
+column_names$V2 <- gsub("\\)", "", column_names$V2)
+column_names$V2 <- gsub("\\(", "", column_names$V2)
+test_data <- read.table("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE, col.names = column_names$V2)
+training_data <- read.table("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE, col.names = column_names$V2)
+# "merge" the data sets... although the instrutions use the term "merge"
+# I believe what is really intended was to append the two data sets
+combined <- rbind(test_data, training_data)
+sel_column_names <- column_names$V2[grep("mean$|mean[^Freq]|std",column_names$V2)]
+subset <- combined[, sel_column_names]
